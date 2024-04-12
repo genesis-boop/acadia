bc8d13cc9d06745e6a1a6a5584bdddf358982a65
unknown <genesisgithub@gmail.com>
2024-04-12 14:59:13 -0700

    Finished wireframing TaskDashboardComponent

--- src/components/TaskDashboardComponent/TaskDashboardComponent.jsx
+++ src/components/TaskDashboardComponent/TaskDashboardComponent.jsx
@@ -6,26 +6,57 @@ import Stack from "@mui/material/Stack";
 import Card from "@mui/material/Card";
 import "./TaskDashboardComponent.css";
 import { Typography } from "@mui/material";
+import "@fontsource/space-grotesk/500.css";
 
 function TaskDashboardComponent() {
   return (
     <Container className="taskDashboard">
       <Stack direction="column">
+        <Typography
+          variant="h4"
+          sx={{
+            fontFamily: "Space Grotesk, sans-serif",
+            fontSize: 24,
+            display: "flex",
+            justifyContent: "flex-start",
+            margin: "8px 8px 0 8px",
+          }}
+          m={2}
+        >
+          CALENDAR
+        </Typography>
+
         <LocalizationProvider dateAdapter={AdapterDayjs}>
           <DateCalendar
             className="taskDashboard-calendar"
             readOnly
             label="Today's Date"
+            sx={{
+              fontFamily: "Space Grotest, sans-serif",
+            }}
           />
         </LocalizationProvider>
-        <Typography>Tasks Today</Typography>
+
+        <Typography
+          variant="h4"
+          sx={{
+            fontFamily: "Space Grotesk, sans-serif",
+            fontSize: 24,
+            display: "flex",
+            justifyContent: "flex-start",
+            margin: "8px",
+          }}
+          m={2}
+        >
+          TODAY'S TASKS
+        </Typography>
         <Card
           sx={{
             height: 80,
             width: "100%",
             backgroundColor: "white",
             marginBottom: 1,
-            boxShadow: "4px 4px 5px rgba(0,0,0,0.2)",
+            boxShadow: "4px 4px 12px rgba(0,0,0,0.2)",
             borderRadius: "12px",
           }}
         />
@@ -35,7 +66,7 @@ function TaskDashboardComponent() {
             width: "100%",
             backgroundColor: "white",
             marginBottom: 1,
-            boxShadow: "4px 4px 5px rgba(0,0,0,0.2)",
+            boxShadow: "4px 4px 12px rgba(0,0,0,0.2)",
             borderRadius: "12px",
           }}
         />
@@ -45,7 +76,7 @@ function TaskDashboardComponent() {
             width: "100%",
             backgroundColor: "white",
             marginBottom: 1,
-            boxShadow: "4px 4px 5px rgba(0,0,0,0.2)",
+            boxShadow: "4px 4px 12px rgba(0,0,0,0.2)",
             borderRadius: "12px",
           }}
         />
