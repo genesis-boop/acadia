eb0b0f7d10a22928710eeff8399d379465ae6288
genesis-boop <genesisgithub@gmail.com>
2024-04-16 00:56:02 -0700

    Added functionality, ToDo page and ToDo Component

---
+++ src/components/ToDoComponent/ToDoComponent.jsx
@@ -0,0 +1,66 @@
+import Card from "@mui/material/Card";
+import Chip from "@mui/material/Chip";
+import Stack from "@mui/material/Stack";
+import "./ToDoComponent.css";
+
+function ToDo({ todo }) {
+  const getChipColor = (tag) => {
+    switch (tag) {
+      case "Urgent":
+        return "error";
+      case "Home":
+        return "warning";
+      case "Work":
+        return "info";
+      case "School":
+        return "success";
+      default:
+        return "default";
+    }
+  };
+
+  return (
+    <Card
+      sx={{
+        maxWidth: "auto",
+        maxHeight: "auto",
+        height: "90px",
+        padding: "8px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+      }}
+    >
+      <div class="checkbox-wrapper-11">
+        <input id={todo.id} type="checkbox" name="r" value="2" />
+        <label for={todo.id}>{todo.title}</label>
+      </div>
+      <Stack
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          gap: "8px",
+        }}
+      >
+        {todo.tags ? (
+          Array.isArray(todo.tags) ? ( // Check if tags is an array
+            todo.tags.map((tag) => (
+              <Chip label={tag} key={tag} color={getChipColor(tag)} />
+            ))
+          ) : (
+            todo.tags
+              .split(",")
+              .map((tag) => (
+                <Chip label={tag} key={tag} color={getChipColor(tag)} />
+              ))
+          )
+        ) : (
+          <></> // Or any other indicator
+        )}
+        <Chip label={"Due: " + todo.dueDate.slice(0, 10)} />
+      </Stack>
+    </Card>
+  );
+}
+
+export default ToDo;
--- src/pages/ToDoPage/ToDoPage.jsx
+++ src/pages/ToDoPage/ToDoPage.jsx
@@ -1,5 +1,109 @@
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Card from "@mui/material/Card";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import ToDo from "../../components/ToDoComponent/ToDoComponent";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 function ToDoPage() {
-  return <></>;
+  const [toDoList, setToDoList] = useState([]);
+
+  useEffect(() => {
+    const getToDos = async () => {
+      try {
+        const response = await axios.get("http://localhost:8080/todo");
+        setToDoList(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getToDos();
+  }, []);
+
+  const taskCounts = {
+    low: toDoList.filter((todo) => todo.priority === 1).length,
+    medium: toDoList.filter((todo) => todo.priority === 2).length,
+    high: toDoList.filter((todo) => todo.priority === 3).length,
+  };
+
+  return (
+    <Container>
+      <Typography>To Do lists</Typography>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-evenly",
+        }}
+      >
+        <Card
+          sx={{
+            width: "350px",
+            height: "650px",
+            backgroundColor: "#E4E4E4",
+            padding: "8px",
+            display: "flex",
+            flexDirection: "column",
+            gap: "12px",
+          }}
+        >
+          <Stack direction="row">
+            <Typography>Low Priority</Typography>
+            <Typography>{taskCounts.low}</Typography>
+          </Stack>
+          {toDoList
+            .filter((todo) => todo.priority === 1)
+            .map((todo) => (
+              <ToDo key={todo.id} todo={todo} />
+            ))}
+        </Card>
+        <Card
+          sx={{
+            width: "350px",
+            height: "650px",
+            backgroundColor: "#E4E4E4",
+            padding: "8px",
+            display: "flex",
+            flexDirection: "column",
+            gap: "12px",
+          }}
+        >
+          <Stack direction="row">
+            <Typography>Medium Priority</Typography>
+            <Typography>{taskCounts.medium}</Typography>
+          </Stack>
+          {toDoList
+            .filter((todo) => todo.priority === 2)
+            .map((todo) => (
+              <ToDo key={todo.id} todo={todo} />
+            ))}
+        </Card>
+        <Card
+          sx={{
+            width: "350px",
+            height: "650px",
+            backgroundColor: "#E4E4E4",
+            padding: "8px",
+            display: "flex",
+            flexDirection: "column",
+            gap: "12px",
+          }}
+        >
+          <Stack direction="row">
+            <Typography>High Priority</Typography>
+            <Typography>{taskCounts.high}</Typography>
+          </Stack>
+          {toDoList
+            .filter((todo) => todo.priority === 3)
+            .map((todo) => (
+              <ToDo key={todo.id} todo={todo} />
+            ))}
+        </Card>
+      </Box>
+    </Container>
+  );
 }
 
 export default ToDoPage;
